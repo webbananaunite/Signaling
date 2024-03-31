6385deef1367ab430875ba8ecb689771138672ca
mYoichi <47585177+webbananaunite@users.noreply.github.com>
2024-03-31 13:27:52 +0900

    Add files via upload

---
+++ READMEjp.md
@@ -0,0 +1,151 @@
+# BlockChain Library Suite
+
+## blocks について
+blocks は中本智さんの論文に基づいて開発されたiOSライブラリです。
+あなたのアプリにブロックチェーンを導入できます。
+
+いろんな目的に使用できますが、仮想通貨取引所で交換可能な暗号資産として使用することを禁止します。
+
+このブロックチェーン・ライブラリが特徴的なのは、政府・公共・民間の社会システムにおける活動をあらかじめ組み込まれていることです。（出生証明、住民票、身元保証など）
+
+添付の overlayNetwork ライブラリに依存します。
+
+## overlayNetwork について
+overlayNetwork は、Peer-to-Peerオーバーレイ・ネットワーク通信システムです。
+MIT Laboratoryの分散ハッシュテーブルの実装である Chord 論文に基づいて開発されたものです。
+
+他のライブラリに依存しません。
+
+## Testy について
+Testy は住民基本台帳カードの代替として開発されています。
+
+blocksライブラリとoverlayNetworkライブラリの使用参考例として作成されたものです。
+
+## Signaling について
+Signalingはオーバーレイネットワークにおいて、NATトラバースを用い、オーバーレイネットワークアドレスをIP/ポートに変換することで、ノード間の通信をコーディネートします。
+Signalingはクラウド上で動作し、ノードの要求に応じ、通信調整を行います。
+Signalingはオーバーレイネットワークにおいて、NAT越えを実現します。
+
+## download
+
+blocks - ブロックチェーン・ライブラリβ版  
+[ダウンロード](https://github.com/webbananaunite/blocks)  
+https://github.com/webbananaunite/blocks  
+ 
+overlayNetwork - peer-to-peer分散ハッシュテーブル通信ライブラリβ版  
+[ダウンロード](https://github.com/webbananaunite/overlayNetwork)  
+https://github.com/webbananaunite/overlayNetwork  
+ 
+Testy - 住民基本台帳アプリβ版  
+[ダウンロード](https://github.com/webbananaunite/Testy)  
+https://github.com/webbananaunite/Testy  
+ 
+Signaling - オーバーレイネットワークアドレスをIP/ポートに変換β版  
+[ダウンロード](https://github.com/webbananaunite/Signaling)  
+https://github.com/webbananaunite/Signaling  
+
+## ライブラリ利用方法
+### Swift Package (推奨)
+1) XcodeでTestyプロジェクトまたはあなたのアプリを開きます。  
+2) File - Add Packages
+3) 右上のSearch or Enter Package URLに次のblocks URLを入力します。
+https://github.com/webbananaunite/blocks
+4) blocks libraryのREADME.mdが表示されます。
+5) 右下のAdd Packageボタンをタップします。
+6) TestyプロジェクトまたはあなたのアプリのプロジェクトのFrameworks, Libraries and Embeded Contentにblocks libraryがあることを確認します。 
+7) TestyプロジェクトまたはあなたのアプリのプロジェクトのProject NavigatorのPackage Dependenciesにブロックチェーン・ライブラリ・スイート(blocks and overlayNetwork libraries)があることを確認します。
+8) 最初の一台は、ブートノードとして起動する必要があります。
+アプリをブートノードとして起動するためには、XcodeのEdit Scheme から RunAsBootNode という名称で Run Argument / Environment Variable のどちらかを設定します。
+9) Xcodeでビルド、デバイスやシミュレータへのインストールを行います。  
+10) アプリを起動し、"Join blocks Network"ボタンをタップします。  
+11) DHCテーブルの初期化が完了するまで8分ほど待ちます。  
+### Carthage
+- $ cd your project directory
+- $ echo 'github "webbananaunite/blocks" "carthage"' > Cartfile
+- $ carthage update --use-xcframeworks
+### CocoaPods
+- $ cd your project directory
+- $ pod init
+- $ vi Podfile
+```
+target 'target name in your App proj' do
+  use_frameworks!
+	pod 'blocks-blockchain'
+end
+```
+- $ pod install
+- Open your app.xcworkspace created by pod.
+
+## 制限事項
+ブートノードを公開アドレス上に作成できていないため、ベータ版を動作させるには、最初のNodeをブートノードとして起動する必要があります。
+
+## ライセンス
+blocks & overlayNetwork & Testy は MIT Licenseで公開されています。  
+
+無料で、あなたのアプリに組み込んで利用できます。  
+いろんな目的のアプリに組み込み可能ですが、仮想通貨取引所で交換可能な暗号資産として使用することを禁止します。
+
+## 禁止事項
+仮想通貨取引所で交換可能な暗号資産として使用することを禁止します。
+
+## その他説明
+### 用語説明:
+- Baby Sitter
+
+あるノードが、OSI参照モデルのセッションレイヤーとしてのオーバーレイネットワークに参加する際には、最初に Baby Sitter NodeをbindサーバーのTXTレコードから見つけ、FSコマンドを送信する必要があります。
+Codeプロトコルによる実装されるDHT（分散ハッシュテーブル）のエントリーごとにです。
+
+- Taker
+
+OSI参照モデルのプレゼンテーションレイヤー／アプリケーションレイヤーとして振る舞うblocksブロックチェーンのネットワークに参加するために、まず最初に行うことは、出生証明書を申請することです。
+このネットワークには、管理者や管理ノードは存在しないため、Takerとなってくれるノードを見つける必要があります。
+AT (Ask For Taker) 要求をトランザクションとしてネットワークに流すためです。
+
+- Book
+
+blocksブロックチェーンが記述されたもの。
+
+- Booker
+
+Bookerとして振る舞うノードは、まだBookされていないトランザクションを収集し、プルーフオブワークによりノンスを計算し、ブロックをネットワークに流します。
+ノンス値が正確な場合、最初のノードがBookerとなり、一時的にブロックチェーン管理者として振舞います。次回のプルーフオブワークまでの期間です。
+
+### 言語:  
+- SwiftUI (Protocol Oriented) 
+- C++ (Metal) 
+- objc (DNS resolv)
+- Python (Signaling)
+
+### サードパーティライブラリの使用
+使用していません。しかし、他の著作物を含んでいます。
+- QuadKey - Microsoft Corporation  
+- SHA-512 - Aaron D. Gifford
+
+### プログラミングアーキテクチャ  
+around DDD, Onion (Protocol Oriented)
+
+### バイトオーダー  
+- Distributed Hash Table (Finger table) address  
+Little Endian
+
+- nonce  
+Little Endian
+
+### cpu, gpu
+nonce の計算はcpuもしくはgpuを選択可能です。
+
+## ステータス
+Beta  
+
+現在、すべての機能は実装されていません。
+- 例. overlayNetwork の DHT ネットワークから離脱したり、また戻ったりする場合への対応。  
+
+2024年5月に、すべての機能を実装した版をリリース予定です。  
+
+Peer-to-Peerオーバーレイ・ネットワークと、ブロックチェーンでの社会基盤構築に賛同していただける方、ボランティアになりますが、共に開発に貢献してくれる方やテストに参加してくれる方を募っています。  
+
+ただし、仮想通貨取引所関係の方はお断りさせていただいておりますことをご了承ください。  
+
+ご連絡をお待ちしています。  
+
+一緒に楽しみましょう！
---
+++ server.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+Based Paper:
+Peer-to-Peer Communication Across Network Address Translators
+
+Thank:
+https://gist.github.com/somic/224795
+https://github.com/laike9m/PyPunchP2P
+"""
+
+import socket
+import struct
+import sys
+from collections import namedtuple
+from select import select
+import time
+import inspect
+
+"""
+Supported 3 NAT Type for Full Cone, Restrict NAT, Restrict Port NAT.
+"""
+FullCone = "Full Cone"  # 0
+RestrictNAT = "Restrict NAT"  # 1
+RestrictPortNAT = "Restrict Port NAT"  # 2
+SymmetricNAT = "Symmetric NAT"  # 3
+UnknownNAT = "Unknown NAT" # 4
+NATTYPE = (FullCone, RestrictNAT, RestrictPortNAT, SymmetricNAT, UnknownNAT)
+
+def log(*args):
+    frame = inspect.currentframe().f_back
+    print(frame.f_code.co_name, frame.f_lineno, time.asctime(), ' '.join([str(x) for x in args]))
+
+poolqueue = {}
+ClientInfo = namedtuple("ClientInfo", "addr, privateaddress, nat_type_id, conn")
+NodeStatus = namedtuple("NodeStatus", "addr, command, data, pool, active, privateaddress, privateip, privateport, nat_type_id, poollength, translateDictionary")
+
+def main():
+    port = sys.argv[1]
+    try:
+        port = int(sys.argv[1])
+    except (IndexError, ValueError):
+        pass
+
+    sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockfd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sockfd.bind(("", port))
+    sockfd.listen(10)
+    sockfd.settimeout(1)    #Timeout secods on accept, recv, send
+    log("listening on *:%d (tcp)" % port)
+    conns = []
+    connsAddrs = {}
+
+    while True:
+        try:
+            log("Wait Connection for Acceptable.")
+            conn = addr = None
+            conn, addr = sockfd.accept()
+        except socket.timeout:
+            log("accept timeout occurred.")
+        if conn != None and addr != None:
+            log('Connectted address: ', addr)
+            conns.append(conn)
+            log(conn)
+            log(addr)
+            connsAddrs[conn] = NodeStatus(addr, None, None, None, None, None, None, None, None, None, None)
+        selectsocket(conns, connsAddrs)
+    log("program end.")
+
+def selectsocket(conns, connsAddrs):
+    log(len(conns))
+    log(len(connsAddrs))
+    if conns == [] or connsAddrs == {}:
+        log("Not There Connected Node.")
+        return
+    r,w,x = select(conns, conns, [], 0)
+    if r:
+        log("r", len(r))
+        for rSocket in r:
+            data = rSocket.recv(1024)
+            log("recv:", data)
+            connsAddrs[rSocket] = connsAddrs[rSocket]._replace(data=data)
+            connsAddrs[rSocket] = connsAddrs[rSocket]._replace(active=True)
+        receivedDataProcess(connsAddrs)
+    if w:
+        log("w", len(w))
+        for wSocket in w:
+            command = connsAddrs[wSocket].command
+            addr = connsAddrs[wSocket].addr
+            log(command)
+            if command == 'registerMe':
+                log("sent okyours")
+                log(connsAddrs[wSocket])
+                ip, port = addr
+                sendBuff = "okyours {0}:{1}".format(ip, port)
+                log(sendBuff)
+                wSocket.sendto(sendBuff.encode(), addr)
+                log("pool={0}, nat_type={1}, ok sent to client".format(connsAddrs[wSocket].pool, NATTYPE[int(connsAddrs[wSocket].nat_type_id)]))
+            elif command == 'okregisterMe':
+                log("sent registerMeAck")
+                sendBuff = "registerMeAck"
+                log(sendBuff)
+                wSocket.sendto(sendBuff.encode(), addr)
+            elif command == 'translate':
+                log("Sent peer address pair to Claim Node.")
+                publicaddress = connsAddrs[wSocket].translateDictionary['publicaddress']
+                privateaddress = connsAddrs[wSocket].translateDictionary['privateaddress']
+                overlayNetworkAddress = connsAddrs[wSocket].translateDictionary['overlayNetworkAddress']
+                publicaddressForPeer = connsAddrs[wSocket].translateDictionary['publicaddressForPeer']
+                privateaddressForPeer = connsAddrs[wSocket].translateDictionary['privateaddressForPeer']
+                connForPeer = connsAddrs[wSocket].translateDictionary['connForPeer']
+                overlayNetworkAddressForPeer = connsAddrs[wSocket].translateDictionary['overlayNetworkAddressForPeer']
+
+                publicip, publicport = publicaddress
+                privateip, privateport = privateaddress
+                log(publicip, publicport, privateip, privateport, overlayNetworkAddress)
+                sendBuff = "translateAck {0} {1} {2} {3} {4}".format(publicip, publicport, privateip, privateport, overlayNetworkAddress)
+                log(sendBuff)
+                wSocket.sendto(sendBuff.encode(), addr)
+
+                log("Sent peer address pair to Peer Node.")
+                publicip, publicport = publicaddressForPeer
+                privateip, privateport = privateaddressForPeer
+                log(publicip, publicport, privateip, privateport, overlayNetworkAddressForPeer)
+                sendBuffForPeer = "translateAck {0} {1} {2} {3} {4}".format(publicip, publicport, privateip, privateport, overlayNetworkAddressForPeer)
+                log(sendBuffForPeer)
+                connForPeer.sendto(sendBuffForPeer.encode(), publicaddress)
+            else:
+                log("else command None")
+            connsAddrs[wSocket] = connsAddrs[wSocket]._replace(command=None)
+
+def poolqueueForPeer(addr):
+    for key in poolqueue:
+        if poolqueue[key].addr == addr:
+            return poolqueue[key], key
+
+"""
+Receive Data, and attach any processes.
+"""
+def receivedDataProcess(connsAddrs):
+    for key in connsAddrs:
+      log(connsAddrs[key].active)
+      if connsAddrs[key].active:
+        connsAddrs[key] = connsAddrs[key]._replace(active=False)
+        data = connsAddrs[key].data
+        addr = connsAddrs[key].addr
+        conn = key
+        log(data)
+        log(addr)
+        log(conn)
+
+        if data.startswith(b"registerMe "):
+            log("received registerMe")
+            log("connection from {}".format(addr))
+            log(data)
+
+            #  data format:
+            #  {private ip} {private port} {nat type id} {address length} {overlayNetworkAddress}null
+            #  ex.
+            #  '192.168.0.34 1402 0 128 8d3a6c0be806ba24b319f088a45504ea7d601970e0f820ca6965eeca1af2d8747d5bdf0ab68a30612004d54b88fe32a654fb7b300568acf8f3e8c6be439c20b9\x00'
+            log("---")
+            log(data.decode().strip())
+            command, privateip, privateport, nat_type_id, poollength, pool = data.decode().strip().split()
+            privateaddress = (privateip, privateport)
+            connsAddrs[key] = connsAddrs[key]._replace(command=command, privateip=privateip, privateport=privateport, nat_type_id=nat_type_id, poollength=poollength, pool=pool, privateaddress=privateaddress)
+            log(connsAddrs[key].command)
+            log(connsAddrs[key].privateaddress)
+            log(connsAddrs[key].nat_type_id)
+            log(connsAddrs[key].poollength)
+            log(connsAddrs[key].pool)
+
+        elif data.startswith(b"okregisterMe"):
+            log("received okregisterMe")
+            log(data, addr)
+            connsAddrs[key] = connsAddrs[key]._replace(command="okregisterMe")
+            log(connsAddrs[key].command)
+            log("request received for pool:", connsAddrs[key].pool)
+            poolqueue[connsAddrs[key].pool] = ClientInfo(addr, connsAddrs[key].privateaddress, connsAddrs[key].nat_type_id, conn)
+
+        elif data.startswith(b"translate "):
+            log("received translate")
+            log("connection from {}".format(addr))
+            log(data)
+            log(data.decode().strip())
+            command, poollength, pool = data.decode().strip().split()
+            log(command)
+            log(poollength)
+            log(pool)
+
+            try:
+                translateDictionary = {}
+                #to claimed node
+                translateDictionary['publicaddress'] = poolqueue[pool].addr
+                translateDictionary['privateaddress'] = poolqueue[pool].privateaddress
+                poolForPeer, overlayNetworkAddressForPeer = poolqueueForPeer(addr)
+                log(poolForPeer, overlayNetworkAddressForPeer)
+                translateDictionary['overlayNetworkAddress'] = pool
+                #to peer node
+                translateDictionary['publicaddressForPeer'] = poolForPeer.addr
+                translateDictionary['privateaddressForPeer'] = poolForPeer.privateaddress
+                translateDictionary['connForPeer'] = poolqueue[pool].conn
+                translateDictionary['overlayNetworkAddressForPeer'] = overlayNetworkAddressForPeer
+
+                connsAddrs[key] = connsAddrs[key]._replace(command=command, translateDictionary=translateDictionary)
+
+            except KeyError:
+                log("connection from {}".format(addr))
+        else:
+            log("received illigal command", data)
+            command = None
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        log("usage: server.py port")
+        exit(0)
+    else:
+        assert sys.argv[1].isdigit(), "port should be a number!"
+        main()
